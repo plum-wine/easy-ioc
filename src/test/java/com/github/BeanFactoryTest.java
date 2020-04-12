df505e74a09942dff67bab95775ef047fa0e9636
plum-wine <1078504296@qq.com>
2020-04-12 10:11:24 +0800

    bean factory

---
+++ src/test/java/com/github/BeanFactoryTest.java
@@ -0,0 +1,53 @@
+package com.github;
+
+import com.github.beans.definition.BeanDefinition;
+import com.github.beans.factory.AbstractBeanFactory;
+import com.github.beans.factory.AutowireCapableBeanFactory;
+import com.github.beans.io.ResourceLoader;
+import com.github.beans.xml.XmlBeanDefinitionReader;
+import com.github.service.HelloWorldService;
+import org.junit.Test;
+
+import java.util.Map;
+
+/**
+ * @author yihua.huang@dianping.com
+ */
+public class BeanFactoryTest {
+
+    @Test
+    public void testLazy() throws Exception {
+        // 1.读取配置
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(new ResourceLoader());
+        xmlBeanDefinitionReader.loadBeanDefinitions("config.xml");
+
+        // 2.初始化BeanFactory并注册bean
+        AbstractBeanFactory beanFactory = new AutowireCapableBeanFactory();
+        for (Map.Entry<String, BeanDefinition> beanDefinitionEntry : xmlBeanDefinitionReader.getRegistry().entrySet()) {
+            beanFactory.registerBeanDefinition(beanDefinitionEntry.getKey(), beanDefinitionEntry.getValue());
+        }
+
+        // 3.获取bean
+        HelloWorldService helloWorldService = (HelloWorldService) beanFactory.getBean("helloWorldService");
+        helloWorldService.helloWorld();
+    }
+
+    @Test
+    public void testPreInstantiate() throws Exception {
+        // 1.读取配置
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(new ResourceLoader());
+        xmlBeanDefinitionReader.loadBeanDefinitions("config.xml");
+
+        // 2.初始化BeanFactory并注册bean
+        AbstractBeanFactory beanFactory = new AutowireCapableBeanFactory();
+        for (Map.Entry<String, BeanDefinition> beanDefinitionEntry : xmlBeanDefinitionReader.getRegistry().entrySet()) {
+            beanFactory.registerBeanDefinition(beanDefinitionEntry.getKey(), beanDefinitionEntry.getValue());
+        }
+
+        // 3.初始化bean
+        beanFactory.preInstantiateSingletons();
+        // 4.获取bean
+        HelloWorldService helloWorldService = (HelloWorldService) beanFactory.getBean("helloWorldService");
+        helloWorldService.helloWorld();
+    }
+}
---
+++ src/test/java/com/github/service/HelloWorldService.java
@@ -0,0 +1,7 @@
+package com.github.service;
+
+public interface HelloWorldService {
+
+    void helloWorld();
+
+}
